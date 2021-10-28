9c96db39a8f5074d92d4c55719cfc4f5820d02e8
viennarose <viennapepito01@gmail.com>
2021-10-28 11:30:42 +0800

    DC Midterm

---
+++ app/src/main/java/com/vienna/midtermapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.vienna.midtermapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton login = (MaterialButton) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                Toast.makeText(MainActivity.this, "Thanks for Logging in!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
